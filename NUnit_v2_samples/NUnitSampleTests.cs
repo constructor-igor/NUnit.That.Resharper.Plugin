5da3a470f7560380a0fd92e354e2a1ffe45a78ba
Igor <igor.zc@gmail.com>
2015-03-18 13:40:43 +0200

    added "nunit sample (v2 and v3)" projects

---
+++ NUnit_v2_samples/NUnitSampleTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace NUnit_v2_samples
+{
+    [TestFixture]
+    public class NUnitSampleTests
+    {
+        [Test]
+        public void AssertIsNullOrEmpty()
+        {
+            string actual = null;
+            Assert.IsNullOrEmpty(actual);
+        }
+    }
+}
---
+++ NUnit_v3_samples/NUnitSampleTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace NUnit_v3_samples
+{
+    [TestFixture]
+    public class NUnitSampleTests
+    {
+        [Test]
+        public void AssertIsNullOrEmpty()
+        {
+            string actual = null;
+            Assert.That(actual, Is.Null.Or.Empty);          // was in nunit v2. Assert.IsNullOrEmpty(actual);
+        }
+    }
+}
