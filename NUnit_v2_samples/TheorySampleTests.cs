db6aafcd306141b8f68b49f2b0f2f5ba454971e6
Igor <igor.zc@gmail.com>
2015-04-19 10:57:23 +0300

    added "TheorySampleTests" files

---
+++ NUnit_v2_samples/TheorySampleTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnit_v2_samples
+{
+    [TestFixture(typeof(int))]
+    [TestFixture(typeof(double))]
+    public class TheorySampleTestsGeneric<T>
+    {
+        [Datapoint]
+        public double[] ArrayDouble1 = { 0, 1, 2, 3 };
+        [Datapoint]
+        public double[] ArrayDouble2 = { 4, 5, 6, 7 };
+        [Datapoint]
+        public int[] ArrayInt = { 0, 1, 2, 3 };
+        [Theory]
+        public void TestGenericForArbitraryArray(T[] array)
+        {
+            Console.WriteLine("TestGenericForArbitraryArray()");
+        }
+    }
+}
