11f4779c618180bebeef6d70faf496a081862769
Igor Ziselman <igor.zc@gmail.com>
2015-08-03 11:42:55 +0300

    added "push" to "release"

--- commands/build.cmd
+++ commands/build.cmd
@@ -1,13 +1,22 @@
 @echo off
 set config=%1
 if "%config%" == "" (
-   set config=Release
+   set config=Debug
 )
 set nuget=nuget\.nuget\NuGet.exe
-set version=0.9.0.5
+set version=0.9.0.6
+set outputDirectory=..\Packages\%config%
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild ..\sources\NUnit.That.Resharper.Plugin.sln /t:Rebuild /p:Configuration="%config%" /m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false 
 
-%nuget% pack "..\sources\NUnit.That.Resharper_v9.Plugin\NUnit.That.Resharper_v9.Plugin.nuspec" -Version %version% -OutputDirectory ..\Packages\%config% -Prop Configuration="%config%" -Symbols -NoPackageAnalysis
+rem
+rem removed "-Symbols" from "nuget pack" 
+rem 
+
+%nuget% pack "..\sources\NUnit.That.Resharper_v9.Plugin\NUnit.That.Resharper_v9.Plugin.nuspec" -Version %version% -OutputDirectory %outputDirectory% -Prop Configuration="%config%" -NoPackageAnalysis
+
+if "%config%" == "Release" (
+	%nuget% push %outputDirectory%\NUnit.That.Resharper_v9.Plugin.%version%.nupkg -Source https://resharper-plugins.jetbrains.com
+)
 
 @echo on
