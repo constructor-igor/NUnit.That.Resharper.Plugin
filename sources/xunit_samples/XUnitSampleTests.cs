0c70a9e4f4300aa68d3d56c810f818f1bc1b7b61
constructor-igor <igor.zc@gmail.com>
2015-11-10 15:03:59 +0200

    added test project xunit_samples

---
+++ sources/xunit_samples/XUnitSampleTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xunit;
+
+namespace xunit_samples
+{
+    public class XUnitSampleTests
+    {
+        [Fact]
+        public void EqualTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void ExpectedExceptionTest()
+        {
+            Assert.ThrowsAny<Exception>(() => Div(2, 0));
+            Assert.Throws<ArgumentException>(() => Div(2, 0));
+        }
+
+        [Fact]
+        //[ExpectedException]
+        public void ExpectedExceptionTestV1()
+        {
+            double r = Div(2, 0);
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        double Div(double x, double y)
+        {
+            if (y==0)
+                throw new ArgumentException();
+            return x/y;
+        }
+    }
+}
